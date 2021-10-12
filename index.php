33bb0db95040ae7bccce40ebd535a9aac576b697
motasjw <lucasm.lopes087@gmail.com>
2021-10-12 17:41:48 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+
+      <title>Controle de Locadora</title>
+
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/style.css">
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <nav class="blue darken-3">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo center">Controle de Locadora</a>
+          <ul id="nav-mobile" class="right">
+            <li class="active"><a href="index.php"><i class="material-icons left">home</i>Menu</a></li>
+          </ul>
+        </div>
+      </nav>
+        
+    </head>
+
+    <body>
+      <div class="container">
+        <div class="row">
+          <ul>
+            <li>
+              <a class="waves-effect waves-light btn">Cadastro de Titulos</a>
+            </li>
+            <li>
+              <a class="waves-effect waves-light btn">Cadastro de Clientes</a>
+            </li>
+            <li>
+              <a class="waves-effect waves-light btn">Controle de Locação</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
