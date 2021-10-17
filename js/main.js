fe58e849fab32e8e2464fbd18420fee5549ab5cb
motasjw <lucasm.lopes087@gmail.com>
2021-10-17 11:49:48 -0300

    Style: Tradução do datepicker/Buttons em negrito

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     $('.datepicker').datepicker({
         format: 'yyyy-mm-dd',
+        i18n: {
+            months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+            monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+            weekdays: ["Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+            weekdaysShort: ["Dom","Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+            weekdaysAbbrev: ["D","S", "T", "Q", "Q", "S", "S"]
+        }
     });
 });
 
